72bc075691b434836f5f4809047b31d0839a9fad
UmarMasroor24 <ummermasroor24@gmail.com>
2020-06-30 00:54:36 +0500

    section 10.4

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -6,4 +6,3 @@
 kaggle competitions download house-prices-advanced-regression-techniques -f test.csv -p packages/regression_model/regression_model/datasets/
      
 kaggle competitions download house-prices-advanced-regression-techniques -f train.csv -p packages/regression_model/regression_model/datasets/
-##
